0c4fc783deef5dbe96d011ced1d57ca32753b3ba
Okubasufrank <franklineokubasu51@gmail.com>
2022-06-23 12:23:03 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+const element = document.getElementById('main');
+element.remove()
+
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.innerHTML = "Okubasu Frankline  is the champion"
